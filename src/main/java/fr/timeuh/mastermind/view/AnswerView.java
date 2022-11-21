ef47f3412390384d8a2b1eb1cdf80b131b823776
Timeuh <timotdu54@gmail.com>
2022-11-21 22:06:52 +0100

    answer view constructor

--- src/main/java/fr/timeuh/mastermind/view/AnswerView.java
+++ src/main/java/fr/timeuh/mastermind/view/AnswerView.java
@@ -2,5 +2,22 @@ package fr.timeuh.mastermind.view;
 
 import javafx.scene.layout.GridPane;
 
+/**
+ * View for seeing if the player's answer is correct or not
+ */
 public class AnswerView extends GridPane {
+
+    /**
+     * Constructs a new answer view
+     */
+    public AnswerView() {
+        init();
+    }
+
+    /**
+     * Initialize the view
+     */
+    private void init(){
+        setMinSize(200,300);
+    }
 }
