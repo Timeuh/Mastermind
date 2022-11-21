053fb15ed301c92e3063b97196ec3e61517e03e7
Timeuh <timotdu54@gmail.com>
2022-11-21 22:02:21 +0100

    pon selection view constructor

--- src/main/java/fr/timeuh/mastermind/view/PonSelectionView.java
+++ src/main/java/fr/timeuh/mastermind/view/PonSelectionView.java
@@ -1,6 +1,31 @@
 package fr.timeuh.mastermind.view;
 
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
+/**
+ * Pon selection view for the player
+ */
 public class PonSelectionView extends GridPane {
+
+    /**
+     * Constructs a new pon selection view
+     */
+    public PonSelectionView() {
+        init();
+    }
+
+    /**
+     * Initialize the view
+     */
+    private void init(){
+        for (int i = 1; i < 8; i++){
+            for (int x = 0; x < 5; x++){
+                Circle grey = new Circle(20, Color.GREY);
+                grey.setStroke(Color.BLACK);
+                add(grey, x, i);
+            }
+        }
+    }
 }
