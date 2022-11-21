b1e7b965422010f8c754554fc1495cf32062e082
Timeuh <timotdu54@gmail.com>
2022-11-21 21:53:01 +0100

    some circles adjustments

--- src/main/java/fr/timeuh/mastermind/view/ColorSelectionView.java
+++ src/main/java/fr/timeuh/mastermind/view/ColorSelectionView.java
@@ -1,6 +1,7 @@
 package fr.timeuh.mastermind.view;
 
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -22,11 +23,14 @@ public class ColorSelectionView extends HBox {
      */
     private void init(){
         setAlignment(Pos.CENTER);
-        Circle red = new Circle(100, Color.RED);
-        Circle green = new Circle(100, Color.GREEN);
-        Circle blue = new Circle(100, Color.BLUE);
-        Circle yellow = new Circle(100, Color.YELLOW);
-        Circle brown = new Circle(100, Color.BROWN);
+        Circle red = new Circle(20, Color.RED);
+        Circle green = new Circle(20, Color.GREEN);
+        Circle blue = new Circle(20, Color.BLUE);
+        Circle yellow = new Circle(20, Color.YELLOW);
+        Circle brown = new Circle(20, Color.BROWN);
         getChildren().addAll(red, green, blue, yellow, brown);
+        for (Node node : getChildren()){
+            if (node instanceof Circle circle) circle.setStroke(Color.BLACK);
+        }
     }
 }
