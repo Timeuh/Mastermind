6e132d43db8a613bac7f84355b4fb3e2b13727c1
Timeuh <timotdu54@gmail.com>
2022-11-21 21:32:28 +0100

    global view constructor

--- src/main/java/fr/timeuh/mastermind/view/GlobalView.java
+++ src/main/java/fr/timeuh/mastermind/view/GlobalView.java
@@ -2,6 +2,46 @@ package fr.timeuh.mastermind.view;
 
 import javafx.scene.layout.BorderPane;
 
+/**
+ * Global view of the application
+ */
 public class GlobalView extends BorderPane {
 
+    private ColorSelectionView colorSelection;
+    private MenuView menu;
+    private TopView top;
+
+    /**
+     * Constructs a new global view
+     */
+    public GlobalView() {
+        this.colorSelection = new ColorSelectionView();
+        this.menu = new MenuView();
+        this.top = new TopView();
+
+        setTop(top);
+        setBottom(colorSelection);
+        setCenter(menu);
+    }
+
+    /**
+     * @return the color selection view
+     */
+    public ColorSelectionView getColorSelection() {
+        return colorSelection;
+    }
+
+    /**
+     * @return the menu view
+     */
+    public MenuView getMenu() {
+        return menu;
+    }
+
+    /**
+     * @return the top view
+     */
+    public TopView getTopView() {
+        return top;
+    }
 }
