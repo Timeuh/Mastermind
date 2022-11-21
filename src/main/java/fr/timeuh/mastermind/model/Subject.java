5f22c725d672d6d3f6c33231c97eb95cf2393a8a
tbrin <timotdu54@gmail.com>
2022-11-21 12:52:40 +0100

    Javadoc for interfaces

--- src/main/java/fr/timeuh/mastermind/model/Subject.java
+++ src/main/java/fr/timeuh/mastermind/model/Subject.java
@@ -2,11 +2,25 @@ package fr.timeuh.mastermind.model;
 
 import fr.timeuh.mastermind.view.Observer;
 
+/**
+ * A subject updates its observers to display real time information
+ */
 public interface Subject {
 
+    /**
+     * Add an observer to the subject's list
+     * @param obs the observer to add
+     */
     public void addObserver(Observer obs);
 
+    /**
+     * Remove an observer from the subject's list
+     * @param obs the observer to remove
+     */
     public void delObserver(Observer obs);
 
+    /**
+     * Notify all the observers of the subject's list, so they can update their content
+     */
     public void notifyObservers();
 }
--- src/main/java/fr/timeuh/mastermind/view/Observer.java
+++ src/main/java/fr/timeuh/mastermind/view/Observer.java
@@ -2,7 +2,14 @@ package fr.timeuh.mastermind.view;
 
 import fr.timeuh.mastermind.model.Subject;
 
+/**
+ * Observer watches for changes in the subject, and updates their content subsequently
+ */
 public interface Observer {
 
+    /**
+     * Update their content
+     * @param subject the subject they are watching
+     */
     public void update(Subject subject);
 }
