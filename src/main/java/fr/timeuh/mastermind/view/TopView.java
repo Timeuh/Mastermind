7229160eccc1db828e3c4f91bfdd40718a3bcebb
Timeuh <timotdu54@gmail.com>
2022-11-21 21:35:10 +0100

    top view constructor

--- src/main/java/fr/timeuh/mastermind/view/TopView.java
+++ src/main/java/fr/timeuh/mastermind/view/TopView.java
@@ -2,5 +2,34 @@ package fr.timeuh.mastermind.view;
 
 import javafx.scene.layout.HBox;
 
+/**
+ * Top view with the pon selection and answer display
+ */
 public class TopView extends HBox {
+
+    private PonSelectionView pons;
+    private AnswerView answers;
+
+    /**
+     * Constructs a new top view
+     */
+    public TopView() {
+        this.pons = new PonSelectionView();
+        this.answers = new AnswerView();
+        getChildren().addAll(pons, answers);
+    }
+
+    /**
+     * @return the pon selection view
+     */
+    public PonSelectionView getPons() {
+        return pons;
+    }
+
+    /**
+     * @return the answer view
+     */
+    public AnswerView getAnswers() {
+        return answers;
+    }
 }
