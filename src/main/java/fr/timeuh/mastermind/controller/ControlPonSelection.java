5b20d89d4de1d8d350a46a6ae19be4bbedce4e88
Timeuh <timotdu54@gmail.com>
2022-11-23 22:16:47 +0100

    methods to get compatible x and y for the model

--- src/main/java/fr/timeuh/mastermind/controller/ControlPonSelection.java
+++ src/main/java/fr/timeuh/mastermind/controller/ControlPonSelection.java
@@ -26,9 +26,38 @@ public class ControlPonSelection implements EventHandler<MouseEvent> {
     @Override
     public void handle(MouseEvent event) {
         if (model.getPhase().equals("PLAYING")){
-            double x = Math.round(event.getX() / 40);
-            double y = Math.round(event.getY() / 55);
-            System.out.println("x : " + x + ", y : " + y);
+            int x = getModelX(event.getX());
+            int y = getModelY(event.getY());
         }
+    }
+
+    /**
+     * Get the x compatible with the model pon
+     * @param currentX the event x
+     * @return the x compatible with the model
+     */
+    private int getModelX(double currentX){
+        if (currentX > 10 && currentX < 50) return 0;
+        if (currentX > 60 && currentX < 100) return 1;
+        if (currentX > 110 && currentX < 150) return 2;
+        if (currentX > 160 && currentX < 200) return 3;
+        if (currentX > 210 && currentX < 250) return 4;
+        return 8;
+    }
+
+    /**
+     * Get the y compatible with the model pon
+     * @param currentY the event y
+     * @return the y compatible with the model
+     */
+    private int getModelY(double currentY){
+        if (currentY > 60 && currentY < 100) return 0;
+        if (currentY > 110 && currentY < 150) return 1;
+        if (currentY > 160 && currentY < 200) return 2;
+        if (currentY > 210 && currentY < 250) return 3;
+        if (currentY > 260 && currentY < 300) return 4;
+        if (currentY > 310 && currentY < 350) return 5;
+        if (currentY > 360 && currentY < 400) return 6;
+        return 8;
     }
 }
